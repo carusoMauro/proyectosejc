a3c6e99ded1490eac43a1314095243f4f464c66c
carusoMauro <mauro.caruso@hotmail.com>
2019-10-12 12:00:01 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from './containers/home-layout/home-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{path: 'home', component: HomeLayoutComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeLayoutComponent } from './containers/home-layout/home-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent
   ],
   imports: [
     BrowserModule,
