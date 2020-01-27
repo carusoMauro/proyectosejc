e6c2dd33eec4630ec72bc3bd0b2821e647ae4b8d
carusoMauro <mauro.caruso@hotmail.com>
2020-01-27 20:59:45 +0100

    nombre

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ecjProyectos'`, () => {
+  it(`should have as title 'ejcproyectos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ecjProyectos');
+    expect(app.title).toEqual('ejcproyectos');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ecjProyectos!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ejcproyectos!');
   });
 });
