74476dbcd9076fd276a95081264916cc6b0052fd
carusoMauro <mauro.caruso@hotmail.com>
2020-02-01 02:29:54 +0100

    Version 1.0 (faltan mails)

--- src/app/containers/portfolio-layout/portfolio-layout.component.ts
+++ src/app/containers/portfolio-layout/portfolio-layout.component.ts
@@ -17,13 +17,23 @@ declare var $ :any;
 export class PortfolioLayoutComponent implements OnInit, AfterViewInit{
   homeNumber: number;
   images: Array<string> = [];
+  numberOfImages: Object;
+  tabDescription: Array<string> = [];
+  arr=Array;
   constructor(private ref: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.homeNumber = 1;
     this.images = ["1", "2", "3", "4", "5", "6", "7", "8"];
-  }
+    this.numberOfImages = {1: 7, 2: 8, 3: 2, 4:2};
+    this.tabDescription = [
+      'Barbacoa 32 m2 con baño - Maldonado (Tiempo estimado 25 días)',
+      'Vivienda 44 m2 Ciudad de la Costa (Tiempo estimado 30 días)',
+      'Baño social 1.15 x 1.72 ( armado en 2 días) - Canelones',
+      'Atelier planta alta  15 M2 ( tiempo estimado 25 días) Costa de Oro'
+    ];
 
+  }
   ngAfterViewInit(): void {
     new WOW().init();
     this.initializePrettyPhoto();
